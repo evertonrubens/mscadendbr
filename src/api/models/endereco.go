39f16c9fe91477000437ec4e591cd6bc0d5789bc
Everton Martins <engeverton@gmail.com>
2023-04-11 21:07:24 -0300

    commit to include new struct at type Endereco. Att. Everton

--- src/api/models/endereco.go
+++ src/api/models/endereco.go
@@ -3,13 +3,13 @@ package models
 //import "time"
 
 type Endereco struct {
-	Id         string
-	NomePF     string
-	Logradouro string
-	Numero     string
-	Bairro     string
-	Cidade     string
-	Uf         string
-	Cep        string
-	DtCriacao  string
+	Id         string  `json: id`
+	NomePF     string  `json: nomePF`
+	Logradouro string  `json: logradouro`
+	Numero     string  `json: numero`
+	Bairro     string  `json: bairro`
+	Cidade     string  `json: cidade`
+	Uf         string  `json: uf`
+	Cep        string  `json: cep`
+	DtCriacao  string  `json: dtCriacao`
 }
